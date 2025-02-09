356a353dac13a95dec0ee086c69e2f57329c7159
Ilir E. <72442005+DerAlbaner180@users.noreply.github.com>
2025-02-10 00:38:34 +0100

    Added Purgatory Flame Item

--- main.lua
+++ main.lua
@@ -64,3 +64,113 @@ function mod:PollenNewRoom()
 end
 
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.PollenNewRoom)
+
+
+-- Item: Purgatory Flame
+
+local PURGATORY_FLAME = Isaac.GetItemIdByName("Purgatory Flame") -- Hole die Item-ID
+local PERMANENT_STATS = {
+    Damage = 0.1,
+    Speed = 0.02,
+    Range = 0.25,
+    Tears = 0.05,
+    Luck = 0.5
+}
+local FIRE_ITEMS = {
+    CollectibleType.COLLECTIBLE_BRIMSTONE,
+    CollectibleType.COLLECTIBLE_SULFUR,
+    CollectibleType.COLLECTIBLE_ABADDON,
+    CollectibleType.COLLECTIBLE_MAW_OF_THE_VOID,
+    CollectibleType.COLLECTIBLE_DARK_MATTER,
+    CollectibleType.COLLECTIBLE_DEATHS_TOUCH,
+    CollectibleType.COLLECTIBLE_SACRIFICIAL_DAGGER,
+    CollectibleType.COLLECTIBLE_GOAT_HEAD,
+    CollectibleType.COLLECTIBLE_PYROMANIAC,
+    CollectibleType.COLLECTIBLE_HOT_BOMBS,
+    CollectibleType.COLLECTIBLE_EXPLOSIVO,
+    CollectibleType.COLLECTIBLE_SMELTER,
+    CollectibleType.COLLECTIBLE_SULFURIC_ACID,
+    CollectibleType.COLLECTIBLE_LOST_CONTACT,
+    CollectibleType.COLLECTIBLE_JACOBS_LADDER,
+    CollectibleType.COLLECTIBLE_CRICKETS_BODY,
+    CollectibleType.COLLECTIBLE_BOBS_ROTTEN_HEAD,
+    CollectibleType.COLLECTIBLE_COMPOUND_FRACTURE,
+    CollectibleType.COLLECTIBLE_HAEMOLACRIA,
+    CollectibleType.COLLECTIBLE_EPIC_FETUS,
+    CollectibleType.COLLECTIBLE_DR_FETUS,
+    CollectibleType.COLLECTIBLE_HOST_HAT,
+    CollectibleType.COLLECTIBLE_SOY_MILK,
+    CollectibleType.COLLECTIBLE_PARASITE,
+    CollectibleType.COLLECTIBLE_IPECAC
+}
+
+function mod:UsePurgatoryFlame(item, rng, player, useFlags, activeSlot, varData)
+    local room = Game():GetRoom()
+    local fireCount = 0
+    local blueFlameCount = 0
+
+    -- Durchsuche alle Entities im Raum
+    for i, entity in pairs(Isaac.GetRoomEntities()) do
+        if entity.Type == EntityType.ENTITY_FIREPLACE then
+            fireCount = fireCount + 1
+
+            if entity.Variant == 1 then -- Blaue Flamme
+                blueFlameCount = blueFlameCount + 1
+            end
+
+            entity:Die() -- Feuer löschen
+        end
+    end
+
+    if fireCount > 0 then
+        -- Wende permanente Stat-Buffs an
+        local data = player:GetData()
+        data.FlamesPurged = (data.FlamesPurged or 0) + fireCount
+
+        player:AddCacheFlags(CacheFlag.CACHE_DAMAGE)
+        player:AddCacheFlags(CacheFlag.CACHE_SPEED)
+        player:AddCacheFlags(CacheFlag.CACHE_RANGE)
+        player:AddCacheFlags(CacheFlag.CACHE_FIREDELAY)
+        player:AddCacheFlags(CacheFlag.CACHE_LUCK)
+        player:EvaluateItems()
+
+        -- Schwarzes Herz oder Devil Room Bonus
+        if data.FlamesPurged >= 50 then
+            player:AddBlackHearts(1)
+        end
+     -- Nach 100 Flammen: Ein Devil Room Item erscheint
+     if data.FlamesPurged >= 100 and data.FlamesPurged - fireCount < 100 then
+        local pos = room:FindFreePickupSpawnPosition(player.Position, 0, true)
+        local chosenItem = FIRE_ITEMS[math.random(#FIRE_ITEMS)]
+        Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, chosenItem, pos, Vector(0, 0), nil)
+    end
+end
+
+return true
+end
+
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UsePurgatoryFlame, PURGATORY_FLAME)
+
+-- **2. Stat-Boost Berechnung**
+function mod:OnEvaluateCache(player, cacheFlag)
+    local data = player:GetData()
+    if data.FlamesPurged then
+        if cacheFlag == CacheFlag.CACHE_DAMAGE then
+            player.Damage = player.Damage + (PERMANENT_STATS.Damage * data.FlamesPurged)
+        end
+        if cacheFlag == CacheFlag.CACHE_SPEED then
+            player.MoveSpeed = player.MoveSpeed + (PERMANENT_STATS.Speed * data.FlamesPurged)
+        end
+        if cacheFlag == CacheFlag.CACHE_RANGE then
+            player.TearRange = player.TearRange + (PERMANENT_STATS.Range * data.FlamesPurged)
+        end
+        if cacheFlag == CacheFlag.CACHE_FIREDELAY then
+            player.MaxFireDelay = math.max(5, player.MaxFireDelay - (PERMANENT_STATS.Tears * data.FlamesPurged))
+        end
+        if cacheFlag == CacheFlag.CACHE_LUCK then
+            player.Luck = player.Luck + (PERMANENT_STATS.Luck * data.FlamesPurged)
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.OnEvaluateCache)
