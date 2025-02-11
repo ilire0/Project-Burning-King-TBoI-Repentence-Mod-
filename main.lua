23d067ac17c639efcaa57eb8e58eeb270e87b58b
Ilir E. <72442005+DerAlbaner180@users.noreply.github.com>
2025-02-11 02:21:49 +0100

    Little Addition so Gabriel works

--- main.lua
+++ main.lua
@@ -177,14 +177,87 @@ end
 
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, mod.OnEvaluateCache)
 
+-- Gabriel Code / Do not change , because its for testing and nothing else , will delete everything below later on!
+-- ###############################################################################################################################################
+
+local gabrielType = Isaac.GetPlayerTypeByName("Gabriel", false) -- Exactly as in the xml. The second argument is if you want the Tainted variant.
+local hairCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_hair.anm2") -- Exact path, with the "resources" folder as the root
+local stolesCostume = Isaac.GetCostumeIdByPath("gfx/characters/gabriel_stoles.anm2") -- Exact path, with the "resources" folder as the root
+
+function MyCharacterMod:GiveCostumesOnInit(player)
+    if player:GetPlayerType() ~= gabrielType then
+        return -- End the function early. The below code doesn't run, as long as the player isn't Gabriel.
+    end
+
+    player:AddNullCostume(hairCostume)
+    player:AddNullCostume(stolesCostume)
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.GiveCostumesOnInit)
 
 
---- King Manasse Custom Item start.
-function mod:OnPlayerInit(player)
-    if player:GetPlayerType() == Isaac.GetPlayerTypeByName("King Manasse", false) then
-        -- Give the custom item
-        player:AddCollectible(Isaac.GetItemIdByName("Purgatory Flame"))
+--------------------------------------------------------------------------------------------------
+
+
+local game = Game() -- We only need to get the game object once. It's good forever!
+local DAMAGE_REDUCTION = 0.6
+function MyCharacterMod:HandleStartingStats(player, flag)
+    if player:GetPlayerType() ~= gabrielType then
+        return -- End the function early. The below code doesn't run, as long as the player isn't Gabriel.
+    end
+
+    if flag == CacheFlag.CACHE_DAMAGE then
+        -- Every time the game reevaluates how much damage the player should have, it will reduce the player's damage by DAMAGE_REDUCTION, which is 0.6
+        player.Damage = player.Damage - DAMAGE_REDUCTION
     end
 end
 
-mod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, mod.OnPlayerInit)
+MyCharacterMod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, MyCharacterMod.HandleStartingStats)
+
+function MyCharacterMod:HandleHolyWaterTrail(player)
+    if player:GetPlayerType() ~= gabrielType then
+        return -- End the function early. The below code doesn't run, as long as the player isn't Gabriel.
+    end
+
+    -- Every 4 frames. The percentage sign is the modulo operator, which returns the remainder of a division operation!
+    if game:GetFrameCount() % 4 == 0 then
+        -- Vector.Zero is the same as Vector(0, 0). It is a constant!
+        local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER_TRAIL, 0, player.Position, Vector.Zero, player):ToEffect()
+        creep.SpriteScale = Vector(0.5, 0.5) -- Make it smaller!
+        creep:Update() -- Update it to get rid of the initial red animation that lasts a single frame.
+    end
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, MyCharacterMod.HandleHolyWaterTrail)
+
+--------------------------------------------------------------------------------------------------
+
+local TAINTED_GABRIEL_TYPE = Isaac.GetPlayerTypeByName("Gabriel", true)
+local HOLY_OUTBURST_ID = Isaac.GetItemIdByName("Holy Outburst")
+local game = Game()
+
+---@param player EntityPlayer
+function MyCharacterMod:TaintedGabrielInit(player)
+    if player:GetPlayerType() ~= TAINTED_GABRIEL_TYPE then
+        return
+    end
+
+    player:SetPocketActiveItem(HOLY_OUTBURST_ID, ActiveSlot.SLOT_POCKET, true)
+
+    local pool = game:GetItemPool()
+    pool:RemoveCollectible(HOLY_OUTBURST_ID)
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_POST_PLAYER_INIT, MyCharacterMod.TaintedGabrielInit)
+
+function MyCharacterMod:HolyOutburstUse(_, _, player)
+    local spawnPos = player.Position
+
+    local creep = Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.PLAYER_CREEP_HOLYWATER, 0, spawnPos, Vector.Zero, player):ToEffect()
+    creep.Scale = 2
+    creep:Update()
+
+    return true
+end
+
+MyCharacterMod:AddCallback(ModCallbacks.MC_USE_ITEM, MyCharacterMod.HolyOutburstUse, HOLY_OUTBURST_ID)
